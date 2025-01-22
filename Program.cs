b59f8769b9d8536164a3bb25a08fd683b53c9a1c
asklootz <askl@uia.no>
2025-01-22 15:13:03 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿int x = 1;
+int userNum;
+
+Console.WriteLine("Enter a number: ");
+userNum = int.Parse(Console.ReadLine());
+
+while (x <= userNum)
+{
+	if (x % 3 == 0 && x % 5 == 0)
+	{
+		Console.WriteLine("FizzBuzz");
+	}
+	else if (x % 3 == 0)
+	{
+		Console.WriteLine("Fizz");
+	}
+	else if (x % 5 == 0)
+	{
+		Console.WriteLine("Buzz");
+	}
+	else
+	{
+		Console.WriteLine(x);
+	}
+	x++;
+};
